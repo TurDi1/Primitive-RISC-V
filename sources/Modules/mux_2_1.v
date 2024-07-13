360aac8e22b7b576bd2f034590becec14809813e
TurDi1 <90939428+TurDi1@users.noreply.github.com>
2024-07-13 14:26:57 +0300

    Add files via upload

---
+++ sources/Modules/mux_2_1.v
@@ -0,0 +1,20 @@
+module mux_2_1
+#(
+   parameter XLEN = 32
+)
+
+(
+   i0,
+   i1,
+   s,
+   f
+);
+
+input    [XLEN-1:0]  i0;   // input bus 0 
+input    [XLEN-1:0]  i1;   // input bus 1
+input                s;    // select input
+output   [XLEN-1:0]  f;    // output selected bus
+
+assign   f = (~s) ? i0 : i1; 
+
+endmodule 
