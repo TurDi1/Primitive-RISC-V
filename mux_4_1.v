032be566fd82f7bb6c022b7a3873dc70179d94c6
TurDi1 <90939428+TurDi1@users.noreply.github.com>
2024-12-03 15:05:22 +0300

    Update mux_4_1.v

--- mux_4_1.v
+++ mux_4_1.v
@@ -1,32 +1,19 @@
 module mux_4_1 
 #(
+   parameter N = 4,
    parameter XLEN = 32
 )
-
 (
    a,
-   b,
-   c,
-   d,
    s,
    f
 );
+// ===== Ports description =====
+input    [XLEN-1:0] a [N - 1 : 0];  // Data input bus
+input    [$clog2(N) - 1 : 0]    s;  // Selector input bus
+output   [XLEN-1:0]             f;  // Data output
 
-input  [XLEN-1:0]  a;
-input  [XLEN-1:0]  b;
-input  [XLEN-1:0]  c;
-input  [XLEN-1:0]  d;
-input  [1:0]       s;
-output reg [XLEN-1:0]  f;
-
-always @ (*)
-begin
-   case(s)
-      2'b00: f = a;
-      2'b01: f = b;
-      2'b10: f = c;
-      2'b11: f = d;
-      default: f = 32'hxxxxxxxx;
-   endcase
-end
+// ===== Assignments =====
+assign f = a[s];
+//
 endmodule 
