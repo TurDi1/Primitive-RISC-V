945f0570466db300da75f3554921e7f51ea89e18
TurDi1 <90939428+TurDi1@users.noreply.github.com>
2025-11-11 20:56:30 +0300

    Updating modules: adder and adder_n_subtractor

--- adder.v
+++ adder.v
@@ -1,18 +1,21 @@
 module adder 
 #(
-   parameter XLEN = 32
+   parameter WIDTH = 32
 )
-
 (
    a,
    b,
    s
 );
+//==================================
+//        PORTS DESCRIPTION
+//==================================
+input   [WIDTH - 1 : 0]  a; // first number input bus
+input   [WIDTH - 1 : 0]  b; // second number input bus
+output  [WIDTH - 1 : 0]  s; // sum of two numbers output bus
 
-input    [XLEN-1:0]  a; // first number input bus
-input    [XLEN-1:0]  b; // second number input bus
-output   [XLEN-1:0]  s; // sum of two numbers output bus
-
+//==================================
+//           ASSIGNMENTS
+//==================================
 assign s = a + b;
-
 endmodule 
--- adder_n_subtractor.v
+++ adder_n_subtractor.v
@@ -1,26 +1,40 @@
 module adder_n_subtractor
 #(
-   parameter XLEN = 32
+   parameter WIDTH = 32
 )
-
 (
    a,
    b,
    c,
    s
 );
+//==================================
+//        PORTS DESCRIPTION
+//==================================
+input   [WIDTH - 1 : 0]  a;   // first number input bus
+input   [WIDTH - 1 : 0]  b;   // second number input bus
+input                    c;   // control inputs
+output  [WIDTH - 1 : 0]  s;   // output bus with result of operation
 
-input         [XLEN-1:0]  a;     // first number input bus
-input         [XLEN-1:0]  b;     // second number input bus
-input                     c;     // control input
-output reg    [XLEN-1:0]  s;     // output bus with result of operation on two numbers
+//==================================
+//      WIRE'S, REG'S and etc
+//==================================
+reg  [WIDTH - 1 : 0]     s_reg;
 
+//==================================
+//           ASSIGNMENTS
+//==================================
+assign s = s_reg;
+
+//==================================
+//              Logic
+//==================================
 always @ (*)
 begin
    case(c)
-      1'b0: s = a + b;
-      1'b1: s = b + ~a + 1;
-      default: s = 32'hxxxxxxxx; 
+      1'b0: s_reg = a + b;
+      1'b1: s_reg = b + ~a + 1;
+      default: s_reg = 32'hxxxxxxxx; 
    endcase
 end
 endmodule 
