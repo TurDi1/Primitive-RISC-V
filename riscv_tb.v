542514d05642f9274d6db8f01d28773102cda99c
TurDi1 <90939428+TurDi1@users.noreply.github.com>
2026-09-16 23:42:29 +0300

    Some edits in tb

--- riscv_tb.v
+++ riscv_tb.v
@@ -4,7 +4,7 @@ module riscv_tb ();
 //==================================
 //           PARAMETERS
 //==================================
-parameter CLK_WIDTH       = 10ns;  // 50 MHz
+parameter CLK_WIDTH       = 10ns;  // 50 MHz. Clock width, half period.
 parameter WIDTH           = 32;    // Data width
 parameter IMEM_ADDR_WIDTH = 5;
 parameter DMEM_ADDR_WIDTH = 5;
@@ -12,7 +12,7 @@ parameter DMEM_ADDR_WIDTH = 5;
 parameter LAST_INSTR_ADDR = 32'h0000001c;
 
 int unsigned rst_time;        // Variable of time for reset
-int unsigned success;         // Success simulation variable
+reg success;                  // Success simulation variable
 
 //==================================
 //      WIRE'S, REG'S and etc
@@ -62,44 +62,65 @@ begin
     $display("TIME:  %t", $realtime);
     $display("-----------------------------------------------");
 
-    fork : waiting_last_instruction
+    fork : check
     begin
-        wait (riscv_single_cycle.instr_addr_o == LAST_INSTR_ADDR);
-        $display("-----------------------------------------------");
-        $display("[TB INFO]  RISCV RECEIVED LAST INSTRUCTION... ");
-        $display("TIME:  %t", $realtime);
-        $display("-----------------------------------------------");
-        $display("");        
-        @(posedge sys_clk_reg);
-        disable waiting_last_instruction;
+        fork
+            begin // Waiting last instruction
+                wait (riscv_single_cycle.instr_addr_o == LAST_INSTR_ADDR);
+                $display("-----------------------------------------------");
+                $display("[TB INFO]  RISCV RECEIVED LAST INSTRUCTION... ");
+                $display("TIME:  %t", $realtime);
+                $display("-----------------------------------------------");
+                $display("");        
+            end
+
+            begin // First check of storing right value to a data memory
+                wait((riscv_single_cycle.mem_addr_o == 32'h00000040) && (riscv_single_cycle.mem_data_o == 32'h00000031) && (riscv_single_cycle.mem_we_o == 1'b1));
+                @(posedge sys_clk_reg);
+                if ((riscv_single_cycle.mem_addr_o == 32'h00000040) && (riscv_single_cycle.mem_data_o == 32'h00000031) && (riscv_single_cycle.mem_we_o == 1'b1))
+                begin
+                    $display("------------------------------------------------------------");
+                    $display("[TB INFO]  ON RISCV MEM PORTS CAPTURED RIGHT WRITE TO DMEM: ");
+                    $display("[TB INFO]  mem_addr_o - %h ", riscv_single_cycle.mem_addr_o);
+                    $display("[TB INFO]  mem_data_o - %h", riscv_single_cycle.mem_data_o);
+                    $display("[TB INFO]  mem_we_o   - %h", riscv_single_cycle.mem_we_o);
+                    $display("------------------------------------------------------------");
+                    success = 1;
+                end
+                else
+                    success = 0;
+            end            
+        join
     end
-    
+
+    // Timeout
     begin
         #1000;
         $display("---------------------------------------------------");
-        $display("[TB ERROR] TIMEOUT: LAST INSTRUCTION NOT REACHED!");
+        $display("[TB ERROR] TIMEOUT: PROGRAM EXECUTION CHECK TIMEOUT");
         $display("TIME:  %t", $realtime);
         $display("---------------------------------------------------");
-        $finish;
+        $fatal;
     end
     join_any
-    
-    // Checking DPRAM register value with address 0x40
-    if (ram.ram[16] == 32'h00000031)
-        success = 1;
-    else
-        success = 0;
-    
+    disable check;
+ 
     $display("");
-    $display("==================== Results of simulation ====================");
+    $display("====================== Results of simulation ======================");
     if (success == 1)
-        $display("==     VALUE IN DPRAM AT ADDRESS 0x40 IS CORRECT, %h ==", ram.ram[16]);
+    begin
+        $display("==              EXPECTED DMEM WRITE TRANSACTION PASSED           ==");
+        $display("===================================================================");
+        $display("");
+        $finish;        
+    end
     else
-        $display("==     VALUE IN DPRAM AT ADDRESS 0x40 IS INCORRECT, %h ==", ram.ram[16]);
-    $display("===============================================================");
-    $display("");
-    
-    $finish;
+    begin
+        $display("==              EXPECTED DMEM WRITE TRANSACTION FAIL            ==");
+        $display("===================================================================");
+        $display("");
+        $fatal;
+    end    
 end
 
 //==================================
